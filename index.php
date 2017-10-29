f9f524f869cd1056a6ad95c5857a18c0cdad9df1
Anthony Willis <anwillis80@gmail.com>
2017-10-29 12:18:16 +0100

    Add files from wamp6

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+		<title>Portfolio</title>
+		<meta charset="UTF-8" />
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Lobster" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+		<link href="style/homepage.style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class = "spacer"></div>
+		<h1>Anthony Willis, Développeur Web PHP Orienté Objet</h1>
+		
+		<div class = "spacer"></div>
+<!--links to be added inside here -->		
+		<div class="listspace">
+			<h2>Liste de Projets</h2>
+			<p>
+				<a href="http://www.willisanthony.fr/parimis" class="noscore">IFOCOP Formation, Parimis Hôtel. <i>HTML, CSS</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/cv" class="noscore">Online CV. <i>HTML, CSS, JQuery</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/parimis2" class="noscore">IFOCOP Formation, Parimis Hôtel Version 2. <i>HTML, CSS</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/parimis3" class="noscore">IFOCOP Formation, Parimis Hôtel. <i>HTML, CSS, PHP</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/annonceo" class="noscore">IFOCOP Formation, Projet Annonceo. <i>HTML, CSS, PHP, SQL</i></a>
+			</p>
+		</div>
+		<div class = "spacer"></div>
+		
+		<div class="listspace">
+			<h2>Contact</h2>
+			
+			<p>
+				<a href="https://www.linkedin.com/in/anthony-willis/" class="noscore">Profile LinkedIn</a>
+			</p>
+		</div>
+		<div class = "spacer"></div>
+		
+		<p class= "footer">
+			c <?php echo date("Y"); ?> Anthony Willis anwillis80@gmail.com<br>
+			Hérbergement: Gandi https://www.gandi.net/ 
+			
+		</p>
+		
+	</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>	
+
+</body>
+</html>
+	
---
+++ style/oldversionindex.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+		<title>Portfolio</title>
+		<meta charset="UTF-8" />
+		<link href="style/homepage.style.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Lobster" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+
+</head>
+<body>
+	<div class="wrapper">
+		<div class = "spacer"></div>
+		<h1>Anthony Willis, Développeur Web PHP Orienté Objet</h1>
+		<div class = "spacer"></div>
+<!--links to be added inside here -->		
+		<div class="listspace">
+			<h2>Liste de Projets</h2>
+			<p>
+				<a href="http://www.willisanthony.fr/parimis" class="noscore">IFOCOP Formation, Parimis Hôtel. <i>HTML, CSS</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/cv" class="noscore">Online CV. <i>HTML, CSS, JQuery</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/parimis2" class="noscore">IFOCOP Formation, Parimis Hôtel Version 2. <i>HTML, CSS</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/parimis3" class="noscore">IFOCOP Formation, Parimis Hôtel. <i>HTML, CSS, PHP</i></a>
+			</p>
+			<p>
+				<a href="http://www.willisanthony.fr/annonceo" class="noscore">IFOCOP Formation, Projet Annonceo. <i>HTML, CSS, PHP, SQL</i></a>
+			</p>
+		</div>
+		<div class = "spacer"></div>
+		<div class="listspace">
+			<h2>Contact</h2>
+			<p>
+				<a href="https://www.linkedin.com/in/anthony-willis/" class="noscore">Profile LinkedIn</a>
+			</p>
+		</div>
+		<div class = "spacer"></div>
+		<p class= "footer">
+			c <?php echo date("Y"); ?> Anthony Willis anwillis80@gmail.com<br>
+			Hérbergement: Gandi https://www.gandi.net/ 
+			
+		</p>
+		
+	</div>
+</body>
+</html>
+	
